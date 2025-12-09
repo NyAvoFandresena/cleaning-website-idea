71061a76e38d17583ce10d315dfed1de0e098aef
NyAvoFandresena <hnafdsn380095@gmail.com>
2025-12-09 21:46:17 +0300

    feat: Add GitHub Pages deployment workflow and Next.js static export configuration

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',  // <=== Enables static exports
+  reactStrictMode: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+  
+  images: {
+    unoptimized: true, // <=== Required for GitHub Pages
+  },
+
+  // !!! IMPORTANT !!!
+  // If your repo is "my-repo", set this to "/my-repo"
+  // If your repo is "username.github.io", delete this line
+  basePath: "/cleaning-website-idea",
 };
 
 export default nextConfig;
